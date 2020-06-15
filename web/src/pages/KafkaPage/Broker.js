b0c4388355007c4538eb9fb6dcd99e8518966f99
Petrovich1973 <designtorg@ya.ru>
2020-06-16 02:03:31 +0300

    mapping broker object response

--- web/src/pages/KafkaPage/Broker.js
+++ web/src/pages/KafkaPage/Broker.js
@@ -39,14 +39,20 @@ const Broker = (props) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [broker])
 
-    // const {
-    //     name = null,
-    //     underReplicated = null,
-    //     inSync = null,
-    //     outOfSync = null,
-    //     bytesInPerSec = null,
-    //     bytesOutPerSec = null
-    // } = broker
+    // console.log(broker)
+
+    const {
+        partitions = {},
+        production = {},
+        consumption = {},
+        system = {}
+    } = broker
+
+    const {
+        cpu = null,
+        disk = null,
+        ram = null,
+    } = system
 
     useEffect(() => {
         props.dispatch({
@@ -77,32 +83,82 @@ const Broker = (props) => {
             </nav>
             &nbsp;
             <div>
-                <table className="table md">
+                {firstReqBroker && Object.keys(broker).length ? <table className="table">
+                    <colgroup>
+                        <col span="1"/>
+                        <col className="col-yellow" span="4"/>
+                        <col className="col-green" span="4"/>
+                        <col className="col-red" span="4"/>
+                        <col className="col-blue" span="3"/>
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <th rowSpan={3}>id</th>
+
+                        <th className="border-bottom opacity" colSpan={4}>partitions</th>
+
+                        <th className="border-bottom opacity" colSpan={4}>production</th>
+
+                        <th className="border-bottom opacity" colSpan={4}>consumption</th>
+
+                        <th className="border-bottom opacity" colSpan={3}>system</th>
+                    </tr>
+                    <tr>
+                        <th rowSpan={2}>total</th>
+                        <th rowSpan={2}>in sync</th>
+                        <th rowSpan={2}>out of sync</th>
+                        <th rowSpan={2}>under replicated</th>
+
+                        <th rowSpan={2}>bytes in per sec</th>
+                        <th colSpan={2} className="border-left border-right opacity">request latency</th>
+                        <th rowSpan={2}>failed request</th>
+                        <th rowSpan={2}>bytes in per sec</th>
+                        <th colSpan={2} className="border-left border-right opacity">request latency</th>
+                        <th rowSpan={2}>failed request</th>
+
+                        <th rowSpan={2}>cpu</th>
+                        <th rowSpan={2}>disk</th>
+                        <th rowSpan={2}>ram</th>
+                    </tr>
+                    <tr>
+                        <th>95th</th>
+                        <th>99.9th</th>
+
+                        <th>95th</th>
+                        <th>99.9th</th>
+                    </tr>
+                    </thead>
                     <tbody>
                     <tr>
-                        <td className="align-right label">
-                            <small>id</small>
-                        </td>
-                        <td>{id}</td>
-                        <td/>
-                        <td className="align-right label">
-                            <small>id</small>
-                        </td>
-                        <td>{id}</td>
-                    </tr>
-                    <tr>
-                        <td className="align-right label">
-                            <small>id</small>
-                        </td>
-                        <td>{id}</td>
-                        <td/>
-                        <td className="align-right label">
-                            <small>id</small>
-                        </td>
-                        <td>{id}</td>
+                        <td className="align-center">{id}</td>
+
+                        <td className="align-center">{partitions.total}</td>
+                        <td className="align-center">{partitions.inSync}</td>
+                        <td className="align-center">{partitions.outOfSync}</td>
+                        <td className="align-center">{partitions.underReplicated}</td>
+
+                        <td className="align-center">{production.bytesInPerSec}</td>
+                        {Object.keys(production.requestLatency).map((key, idxL) => (
+                            <td className="align-center"
+                                key={idxL}>{production.requestLatency[key]}</td>
+                        ))}
+                        <td className="align-center">{production.faileRequests}</td>
+
+                        <td className="align-center">{consumption.bytesInPerSec}</td>
+                        {Object.keys(consumption.requestLatency).map((key, idxL) => (
+                            <td className="align-center"
+                                key={idxL}>{consumption.requestLatency[key]}</td>
+                        ))}
+                        <td className="align-center">{consumption.faileRequests}</td>
+
+                        <td className="align-center">{cpu}</td>
+                        <td className="align-center">{disk}</td>
+                        <td className="align-center">{ram}</td>
                     </tr>
                     </tbody>
-                </table>
+                </table> : firstReqBroker && !Object.keys(broker).length ?
+                    <div className="waiting">ничего не найдено</div> :
+                    <div className="waiting">waiting brokers...</div>}
             </div>
             &nbsp;
             <nav className="tabs">
